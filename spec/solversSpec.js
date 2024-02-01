d0ef2f06d3f48712de758baab4569c5ae105eef0
jreagant <jr.tankersley@gmail.com>
2024-02-01 01:28:34 -0600

    added solvers strategy notes

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -4,7 +4,7 @@ describe('solvers', function() {
   describe('findNRooksSolution()', function() {
 
     it('finds a valid solution for n of 1-8', function() {
-      _.range(1, 9).map(function(n) {
+      _.range(1, 9).map(function(n) { // array 1-8    //map array
         var solutionBoard = new Board(findNRooksSolution(n));
         var numPieces = _.reduce(solutionBoard.rows(), function(memo, row) {
           return memo + _.reduce(row, function(memo, col) {
@@ -25,7 +25,7 @@ describe('solvers', function() {
     it('finds the number of valid solutions for n of 1-8', function() {
       _.range(1, 9).map(function(n) {
         var solutionCount = countNRooksSolutions(n);
-        var expectedSolutionCount = [1, 1, 2, 6, 24, 120, 720, 5040, 40320][n];
+        var expectedSolutionCount = [1, 1, 2, 6, 24, 120, 720, 5040, 40320][n]; //n*1 n*2 n*3 n*4// 1! 2! 3! 4!
 
         expect(solutionCount).to.be.equal(expectedSolutionCount);
       });
--- src/solvers.js
+++ src/solvers.js
@@ -14,9 +14,28 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 
+//returning an array of arrays (the board)
 
-window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+//the files call new Board(findNRooksSolution(n)) // places all n rooks where they dont conflict explicitly with 1
+// create a board in findNRooksSolution and findNQueensSolution
+
+// STRATEGY
+  // make board
+  // increment to n pieces
+    // toggle
+    // if there are row and column conflicts at that coordinate,
+      // untoggle
+
+
+// give findNRooksSolution coordinate parameter, add a line that defaults it to the first position (0, 0)
+
+window.findNRooksSolution = function(n, position) {//position [0][x]
+  position = position || 0;
+  var solution = undefined;
+
+  //solution = new Board({n: n})
+  //for loop i = 0 while i < n
+    //this.togglepiece(i, i)
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
@@ -25,7 +44,7 @@ window.findNRooksSolution = function(n) {
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
   var solutionCount = undefined; //fixme
-
+  // iterate n times through findNRookSolution(n, i)
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
